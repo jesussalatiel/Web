07fc0e13ac3524916cf71d29dc0fd80d6fe51c5d
Jesus Salatiel <jesussalatiel@hotmail.com>
2019-06-20 01:23:44 -0500

    Modify tables

--- Views/Components/Inventory/BarraHueca.php
+++ Views/Components/Inventory/BarraHueca.php
@@ -1,27 +1,36 @@
 <div class="table-responsive">
 <table class="table table-bordered">
-<caption>Barra Hueca</caption>
+<caption class="text-center">Barra Hueca</caption>
   <tr>
-    <td colspan="2">Enero</td>
-    <td colspan="2">Febrero</td>
-	<td rowspan="2">Kg/m</td>
+    <td rowspan="2">Medida</td>
+    <td colspan="2">Diametro Exterior</td>
+	<td rowspan="2">Cedula</td>
+	<td colspan="2">Pared</td>
+	<td colspan="2">Diametro Interior</td>
+	<td rowspan="2">KG x M</td>
 	<td rowspan="2">Peso x Pieza</td>
 	<td rowspan="2">Peso Total</td>
 	<td rowspan="2">Piezas</td>
 	<td rowspan="2">Longitud</td>
   </tr>
   <tr>
-    <td>Exterior</td>
-    <td>Interior</td>
+    <td>in</td>
     <td>mm</td>
     <td>in</td>
+    <td>mm</td>
+	<td>in</td>
+    <td>mm</td>
 	
   </tr>
   <tr>
     <td>1.000€</td>
-    <td>700€/td>
+    <td>700€</td>
     <td>1.100€</td>
     <td>580€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
 	<td>1.000€</td>
 	<td>1.000€</td>
 	<td>1.000€</td>
--- Views/Components/Inventory/Tuberia.php
+++ Views/Components/Inventory/Tuberia.php
@@ -1,1 +1,42 @@
-<h1>Otro</h1>
+<div class="table-responsive">
+<table class="table table-bordered">
+<caption class="text-center">Tuberia</caption>
+  <tr>
+    <td rowspan="2">Medida</td>
+    <td colspan="2">Diametro Exterior</td>
+	<td rowspan="2">Cedula</td>
+	<td colspan="2">Pared</td>
+	<td colspan="2">Diametro Interior</td>
+	<td rowspan="2">KG x M</td>
+	<td rowspan="2">Peso x Pieza</td>
+	<td rowspan="2">Peso Total</td>
+	<td rowspan="2">Piezas</td>
+	<td rowspan="2">Longitud</td>
+  </tr>
+  <tr>
+    <td>in</td>
+    <td>mm</td>
+    <td>in</td>
+    <td>mm</td>
+	<td>in</td>
+    <td>mm</td>
+	
+  </tr>
+  <tr>
+    <td>1.000€</td>
+    <td>700€</td>
+    <td>1.100€</td>
+    <td>580€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+	<td>1.000€</td>
+  </tr>
+ 
+</table>
+</div>
